972f7d79f98d58936b9645f9beee26de3373b620
millni <i1683u@gmail.com>
2020-05-20 08:28:39 +0900

    p156 각 필드를 분리해서 보여주기

--- polls/admin.py
+++ polls/admin.py
@@ -5,7 +5,10 @@ from django.contrib import admin
 from polls.models import Question, Choice
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']  # 필드 순서 변경
+    fieldsets = [
+        ('Question Statement',{'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date']}),
+    ]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
