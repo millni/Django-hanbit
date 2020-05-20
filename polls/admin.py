6b44c1336c1cce4808498c12d0e182ef2b38cf3d
millni <i1683u@gmail.com>
2020-05-20 17:47:57 +0900

    p159 Question 및 Choice를 한 화면에서 보여주기/ p160 테이블 형식

--- polls/admin.py
+++ polls/admin.py
@@ -4,11 +4,17 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Question Statement',{'fields': ['question_text']}),
         ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
