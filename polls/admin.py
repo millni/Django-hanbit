bbc3d27c72ab1a5134410222d3cbacd9ecb09e19
millni <i1683u@gmail.com>
2020-05-20 18:33:11 +0900

    p164 polls-admin.py 정리

--- polls/admin.py
+++ polls/admin.py
@@ -14,7 +14,10 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Question Statement',{'fields': ['question_text']}),
         ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
-    inlines = [ChoiceInline]
+    inlines = [ChoiceInline]                        # Choice 모델 클래스 같이 보기
+    list_display = ('question_text', 'pub_date')    # 레코드 리스트 컬럼 지정
+    list_filter = ['pub_date']                      # 필터 사이드 바 추가
+    search_fields = ['question_text']               # 검색 박스 추가
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
