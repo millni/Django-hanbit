730679e049e2a297509fdf103fc3071040f41373
millni <i1683u@gmail.com>
2020-06-03 07:19:30 +0900

    p228 mysite-urls.py books-urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('polls/', views.index, name='index'),
     path('polls/', include('polls.urls')),
+    path('books/', include('books.urls')),  # 추가
     # path('polls/<int:question_id>/', views.detail, name='detail'),
     # path('polls/<int:question_id>/results/', views.results, name='results'),
     # path('polls/<int:question_id>/vote/', views.vote, name='vote'),
