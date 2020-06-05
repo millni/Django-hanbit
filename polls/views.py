9091b28ac7d96a365f32b91c465b9655094c897a
millni <i1683u@gmail.com>
2020-06-05 16:48:50 +0900

    p261 polls-views.py

--- polls/views.py
+++ polls/views.py
@@ -23,8 +23,13 @@ class ResultView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+# logging 추가
+import logging
+logger = logging.getLogger(__name__)
+
 #-- Function-based View
 def vote(request, question_id):
+    logger.debug("vote().question_id: %s" % question_id)    # 추가
     question =get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
